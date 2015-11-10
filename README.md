ccdf0546aee8346bd0a1fa5813b60326cf141e4c
Charles Cross <charles@openrov.com>
2015-11-10 15:11:48 -0800

    Updated readme with instructions, added node-gyp as dependency, and made code clearer in cpp

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # skeppleton
-Skeleton C++ Node.js Addon for setting up a background thread communicating with a node app through bindings. 
 
 This is initial boilerplate code, entirely temporary, and is not yet fit for human eyes. Beware.
+
+Just run:
+npm install
+node index.js
--- src/skeppleton.cpp
+++ src/skeppleton.cpp
@@ -1,115 +1,3 @@
-// #include <unistd.h>
-// #include <node.h>
-// #include <string.h>
-// #include <v8.h>
-
-// using namespace v8;
-
-// unsigned long long count = 0;
-
-// // native blocking/compute intensive function
-// void delay(int seconds) 
-// {
-//     int i;
-//     int j;
-
-//     // a long computation
-//     for(i=0;i<2000000;++i) 
-//     {
-//         for(j=0;j<400;++j)   
-//         {
-//             count = count * seconds;
-//         }
-//     }
-// }
-
-
-// // the 'baton' is the carrier for data between functions
-// struct DelayBaton
-// {
-//     // required
-//     uv_work_t request;                  // libuv
-//     Persistent<Function> callback;      // javascript callback
-
-//     // optional : data goes here.
-//     // data that doesn't go back to javascript can be any typedef
-//     // data that goes back to javascript needs to be a supported type
-//     int         seconds;
-//     char        greeting[256];
-// };
-
-// // called by libuv worker in separate thread
-// static void DelayAsync(uv_work_t *req)
-// {
-//     DelayBaton *baton = static_cast<DelayBaton *>(req->data);
-//     delay(baton->seconds);
-// }
-
-// // called by libuv in event loop when async function completes
-// static void DelayAsyncAfter(uv_work_t *req,int status)
-// {
-//     // get the reference to the baton from the request
-//     DelayBaton *baton = static_cast<DelayBaton *>(req->data);
-
-//     // set up return arguments
-//     Handle<Value> argv[] =
-//         {
-//             Handle<Value>(Int32::New(baton->seconds)),
-//             Handle<Value>(String::New(baton->greeting))
-//         };
-
-//     // execute the callback
-//     baton->callback->Call(Context::GetCurrent()->Global(),2,argv);
-
-//     // dispose the callback object from the baton
-//     baton->callback.Dispose();
-
-//     // delete the baton object
-//     delete baton;
-// }
-
-// // javascript callable function
-// Handle<Value> Delay(const Arguments &args)
-// {
-//     // create 'baton' data carrier
-//     DelayBaton *baton = new DelayBaton;
-
-//     // get callback argument
-//     Handle<Function> cb = Handle<Function>::Cast(args[2]);
-
-//     // attach baton to uv work request
-//     baton->request.data = baton;
-
-//     // assign incoming arguments to baton
-//     baton->seconds =   args[0]->Int32Value();
-
-//     // point at the argument as a string, then copy it to the baton
-//     v8::String::Utf8Value str(args[1]);
-//     strncpy(baton->greeting,*str,sizeof(baton->greeting));
-
-//     // assign callback to baton
-//     baton->callback = Persistent<Function>::New(cb);
-
-//     // queue the async function to the event loop
-//     // the uv default loop is the node.js event loop
-//     uv_queue_work(uv_default_loop(),&baton->request,DelayAsync,DelayAsyncAfter);
-
-//     // nothing returned
-//     return Undefined();
-// }
-
-// void init( Handle<Object> exports ) 
-// {
-
-//   // add the async function to the exports for this object
-//   exports->Set(
-//                 String::NewSymbol("delay"),                          // javascript function name
-//                 FunctionTemplate::New(Delay)->GetFunction()          // attach 'Delay' function to javascript name
-//               );
-// }
-
-// NODE_MODULE(skeppleton, init)
-
 #include <nan.h>
 #include <unistd.h>
 
@@ -161,32 +49,42 @@ namespace skep
         fprintf( stderr, "BG Thread running...\n" );
         
         int command = 1;
+	bool keepRunning = true;
         
-        while( true ) 
+        while( keepRunning ) 
         {
-            // Update command
+	    //////////////////////////////////////////
+	    // Background processing stuff goes here!
+	    //////////////////////////////////////////
+	  
+            // Get latest command
             uv_mutex_lock( &shared->commandMutex );
             command = shared->currentCommand;
             uv_mutex_unlock( &shared->commandMutex );
             
-            // Update data
+            // Update shared data based on command value
             uv_mutex_lock( &shared->commandMutex );
             
-            // Force break if value is 10
             if( command != 10 )
             {
                 shared->currentData = command * 10;
             }
             else
             {
+		// Force break if value is 10
                 shared->currentData = 999;
-                break;
+                keepRunning = false;
             }
             
             uv_mutex_unlock( &shared->commandMutex );
             
             // Signal main thread that we did some work
             uv_async_send( &shared->async );
+	    
+	    if( !keepRunning )
+	    {
+		break;
+	    }
             
             sleep( 1 );
         }
